4caa3a9d0170616f2a09ed86bd6aa81ec201a109
sas8254 <shashibhushansingh11@gmail.com>
2022-10-27 12:29:02 +0530

    460 Defining our farm & product model

--- models/farm.js
+++ models/farm.js
@@ -1,58 +1,23 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-mongoose
-  .connect("mongodb://localhost:27017/relationshipDemo1")
-  .then(() => {
-    console.log("mongo connection open");
-  })
-  .catch((e) => {
-    console.log("mongo connection error", e);
-  });
 
-const productSchema = new Schema({
-  name: String,
-  price: Number,
-  season: {
+const farmSchema = new Schema({
+  name: {
     type: String,
-    enum: ["Spring", "Summer", "Fall", "Winter"],
+    required: [true, "Farm must have a name"],
+  },
+  city: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Email required"],
+  },
+  products: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
   },
 });
 
-const farmSchema = new Schema({
-  name: String,
-  city: String,
-  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
-});
-
-const Product = mongoose.model("Product", productSchema);
 const Farm = mongoose.model("Farm", farmSchema);
-
-// Product.insertMany([
-//   { name: "Goddess Melon", price: 1.99, season: "Summer" },
-//   { name: "Suger Baby Watermelon", price: 0.99, season: "Summer" },
-//   { name: "Asparagus", price: 3.99, season: "Spring" },
-// ]);
-
-const makeFarm = async () => {
-  const farm = new Farm({ name: "Full Belly Farms", city: "Bharuch" });
-  const melon = await Product.findOne({ name: "Goddess Melon" });
-  farm.products.push(melon);
-  await farm.save();
-  console.log(farm);
-};
-
-// makeFarm();
-
-const addProduct = async () => {
-  const farm = await Farm.findOne({ name: "Full Belly Farms" });
-  const Watermelon = await Product.findOne({ name: "Suger Baby Watermelon" });
-  farm.products.push(Watermelon);
-  await farm.save();
-  console.log(farm);
-};
-
-// addProduct();
-
-Farm.findOne({ name: "Full Belly Farms" })
-  .populate("products")
-  .then((farm) => console.log(farm));
+module.exports = Farm;

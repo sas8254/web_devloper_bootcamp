87df2d2bea518a2d691ab3e88386d2753336b5d4
sas8254 <shashibhushansingh11@gmail.com>
2022-10-28 19:20:51 +0530

    465 finishing touches

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/farms/new", (req, res) => {
 });
 
 app.get("/farms/:id", async (req, res) => {
-  const farm = await Farm.findById(req.params.id);
+  const farm = await Farm.findById(req.params.id).populate("products");
   res.render("farms/show", { farm });
 });
 
@@ -48,9 +48,10 @@ app.post("/farms", async (req, res) => {
   res.redirect("/farms");
 });
 
-app.get("/farms/:id/products/new", (req, res) => {
+app.get("/farms/:id/products/new", async (req, res) => {
   const { id } = req.params;
-  res.render("products/new", { categories, id });
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
 });
 
 app.post("/farms/:id/products", async (req, res) => {
@@ -62,7 +63,7 @@ app.post("/farms/:id/products", async (req, res) => {
   product.farm = farm;
   await farm.save();
   await product.save();
-  res.send(farm);
+  res.redirect(`/farms/${id}`);
 });
 // PRODUCT ROUTES
 

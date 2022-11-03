93e8dd6015609e3caf2facb8a70898f2b947e570
shashibhushan <shashibhushansingh11@gmail.com>
2022-11-04 05:24:59 +0530

    481 signing cookies.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(cookieParser('secretKey'));
 
 app.get("/greet", (req, res) => {
   const { name = "no name" } = req.cookies;
@@ -14,6 +14,14 @@ app.get("/setname", (req, res) => {
   res.send("sent you a cookie");
 });
 
+app.get('/getsignedcookie', (req, res)=>{
+  res.cookie('fruit','apple',{signed:true});
+  res.send('sent you our signed fruit cookie')
+})
+
+app.get('/verifyfruit', (req, res)=>{
+  res.send(req.signedCookies)
+})
 app.listen(4000, () => {
   console.log("Serving on port 4000");
 });

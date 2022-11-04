21383525555683f52bae54ea1b3d82624438d0e1
sas8254 <shashibhushansingh11@gmail.com>
2022-11-04 11:01:57 +0530

    485 Express session

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-app.use(cookieParser('secretKey'));
+const session = require("express-session");
+app.use(
+  session({
+    secret: "thisisnotagoodsecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get("/greet", (req, res) => {
-  const { name = "no name" } = req.cookies;
-  res.send(`Hi there ${name}`);
+app.get("/viewcount", (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You have viewed this page ${req.session.count} times`);
 });
 
-app.get("/setname", (req, res) => {
-  res.cookie("name", "sammual colt");
-  res.cookie("fish", "goldfish");
-  res.send("sent you a cookie");
-});
-
-app.get('/getsignedcookie', (req, res)=>{
-  res.cookie('fruit','apple',{signed:true});
-  res.send('sent you our signed fruit cookie')
-})
-
-app.get('/verifyfruit', (req, res)=>{
-  res.send(req.signedCookies)
-})
 app.listen(4000, () => {
   console.log("Serving on port 4000");
 });

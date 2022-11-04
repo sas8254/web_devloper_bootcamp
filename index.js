3d146a119bd69e5bbd51fec648b23fa84e9875d6
sas8254 <shashibhushansingh11@gmail.com>
2022-11-04 12:56:26 +0530

    487 intro to flash

--- index.js
+++ index.js
@@ -1,34 +1,133 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const session = require("express-session");
-app.use(
-  session({
-    secret: "thisisnotagoodsecret",
-    resave: false,
-    saveUninitialized: false,
+const flash = require("connect-flash");
+
+const Product = require("./models/product");
+const Farm = require("./models/farm");
+const categories = ["fruit", "vegetable", "dairy"];
+
+mongoose
+  .connect("mongodb://localhost:27017/flashDemo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(
+  session({ secret: "notagoodsecret", resave: false, saveUninitialized: false })
 );
+app.use(flash());
 
-app.get("/viewcount", (req, res) => {
-  if (req.session.count) {
-    req.session.count += 1;
+// FARM ROUTES
+
+app.get("/farms", async (req, res) => {
+  const farms = await Farm.find({});
+  res.render("farms/index", { farms, messages: req.flash("success") });
+});
+app.get("/farms/new", (req, res) => {
+  res.render("farms/new");
+});
+app.get("/farms/:id", async (req, res) => {
+  const farm = await Farm.findById(req.params.id).populate("products");
+  res.render("farms/show", { farm });
+});
+
+app.delete("/farms/:id", async (req, res) => {
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+
+  res.redirect("/farms");
+});
+
+app.post("/farms", async (req, res) => {
+  const farm = new Farm(req.body);
+  await farm.save();
+  req.flash("success", "Successfully made a farm!");
+  res.redirect("/farms");
+});
+
+app.get("/farms/:id/products/new", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
+});
+
+// PRODUCT ROUTES
+
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
   } else {
-    req.session.count = 1;
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
   }
-  res.send(`You have viewed this page ${req.session.count} times`);
 });
 
-app.get("/ragister", (req, res) => {
-  const { username = "Anonymous" } = req.query;
-  req.session.username = username;
-  res.redirect("/greet");
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
 });
 
-app.get("/greet", (req, res) => {
-  const { username } = req.session;
-  res.send(`Welcome back ${username}`);
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
 });
 
-app.listen(4000, () => {
-  console.log("Serving on port 4000");
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id).populate("farm", "name");
+  res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+app.listen(3000, () => {
+  console.log("APP IS LISTENING ON PORT 3000!");
 });

00fd6ff90e853b8bebeb9bb21e7bc15003c75f06
sas8254 <shashibhushansingh11@gmail.com>
2022-11-08 16:02:39 +0530

    508. Auth Demo: Refactoring To Model Methods

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
 app.get("/", async (req, res) => {
   res.send("This is home page");
 });
@@ -34,11 +41,7 @@ app.get("/register", async (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const hash = await bcrypt.hash(password, 12);
-  const user = new User({
-    username,
-    password: hash,
-  });
+  const user = new User({ username, password });
   await user.save();
   req.session.user_id = user._id;
   res.redirect("/secret");
@@ -55,21 +58,21 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    req.session.user_id = user._id;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
     res.redirect("/secret");
   } else {
     res.redirect("/login");
   }
 });
 
-app.get("/secret", (req, res) => {
-  if (!req.session.user_id) {
-    return res.redirect("/login");
-  }
+app.get("/secret", requireLogin, (req, res) => {
   res.render("secret");
+});
+
+app.get("/topsecret", requireLogin, (req, res) => {
+  res.send("topsecret");
 });
 
 app.listen(3000, () => {
